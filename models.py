327b10c958e2574fabffe8d0d7c8ccc44aad4688
Sergey Khrustalev <sergey@khrustalev.net>
2017-11-23 12:21:38 +0300

    add advertisement model

--- models.py
+++ models.py
@@ -3,6 +3,16 @@ from app import db
 class Advertisement(db.Model):
     
     __tablename__ = 'advertisements'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    settlement = db.Column(db.String(127))
+    under_construction = db.Column(db.Boolean)
     description = db.Column(db.Text)
+    price = db.Column(db.Integer)
+    oblast_district = db.Column(db.String(127))
+    living_area = db.Column(db.Float)
+    has_balcony = db.Column(db.Boolean)
+    address = db.Column(db.String(255))
+    construction_year = db.Column(db.Integer)
+    rooms_number = db.Column(db.Integer)
+    premise_area = db.Column(db.Float)
     active = db.Column(db.Boolean, default=True)
