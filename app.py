00129f089a697703943c67fdab62f8182c41ddcb
Sergey Khrustalev <sergey@khrustalev.net>
2017-11-23 13:00:53 +0300

    first demo version

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///advertisements.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
--- server.py
+++ server.py
@@ -1,23 +1,13 @@
-from flask import Flask, render_template
+from flask import render_template
+from app import app, db
+from models import Advertisement
 
-app = Flask(__name__)
 
 @app.route('/')
 def ads_list():
-    return render_template('ads_list.html', ads=[{
-            "settlement": "Череповец",
-            "under_construction": False,
-            "description": '''Квартира в отличном состоянии. Заезжай и живи!''',
-            "price": 2080000,
-            "oblast_district": "Череповецкий район",
-            "living_area": 17.3,
-            "has_balcony": True,
-            "address": "Юбилейная",
-            "construction_year": 2001,
-            "rooms_number": 2,
-            "premise_area": 43.0,
-        }]*10
-    )
+
+    ads = Advertisement.query.all()
+    return render_template('ads_list.html', ads=ads)
 
 if __name__ == "__main__":
     app.run()
