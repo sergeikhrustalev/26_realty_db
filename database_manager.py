94a2a7f06e2d9d5bba4a56ba8d35597e81e75523
Sergey Khrustalev <sergey@khrustalev.net>
2017-11-23 12:21:03 +0300

    first release

--- database_manager.py
+++ database_manager.py
@@ -1,35 +1,62 @@
+import argparse
+import json
+
+
 from app import db
 from models import Advertisement
 
 
-db.drop_all()
-db.create_all()
+def get_ads_from_json(filepath):
+
+    with open(filepath) as file_handler:
+        return json.load(file_handler)
 
 
-args = list()
-args.append(dict(description='test 1'))
-args.append(dict(description='test 2'))
+def create_new_base():
+
+    db.drop_all()
+    db.create_all()
 
 
-db.session.add_all(
-    [
-        Advertisement(**arg)
-        for arg in args
-    ]
-)
+def load_ads(ads):
 
-db.session.commit()
+    db.session.add_all([
 
+        Advertisement(**ad)
+        for ad in ads
 
-Advertisement.query.filter_by(
+    ])
 
-    active=True
-
-).update(
-
-    dict(active=False)
-
-)
+    db.session.commit()
 
 
-db.session.commit()
+def deactivate_ads():
+
+    Advertisement.query.filter_by(
+        active=True
+    ).update(
+        dict(active=False)
+    )
+
+    db.session.commit()
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--newbase', action='store_true')
+    parser.add_argument('--loaddata')
+
+    args = parser.parse_args()
+
+    if args.newbase:
+        create_new_base()
+
+    if args.loaddata:
+
+        deactivate_ads()
+
+        load_ads(
+            get_ads_from_json(args.loaddata)
+        )
