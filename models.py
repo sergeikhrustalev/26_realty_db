0127fc72c177134b19df004bd3854ad3aa427218
Sergey Khrustalev <sergey@khrustalev.net>
2017-11-23 17:19:45 +0300

    first release

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 from app import db
 
+
 class Advertisement(db.Model):
-    
+
     __tablename__ = 'advertisements'
     id = db.Column(db.Integer, primary_key=True, autoincrement=False)
     settlement = db.Column(db.String(127))
--- server.py
+++ server.py
@@ -1,19 +1,60 @@
-from flask import render_template
+from datetime import date
+
+from sqlalchemy.sql.expression import true
+from sqlalchemy import or_
+from flask import render_template, request, url_for
 from app import app, db
 from models import Advertisement
 
 
 @app.route('/')
-@app.route('/page/<int:page>')
-def ads_list(page=1):
+def ads_list():
+
+    ads = Advertisement.query.filter(Advertisement.active == true())
+
+    oblast_district = request.args.get('oblast_district')
+    min_price = request.args.get('min_price', None, type=int)
+    max_price = request.args.get('max_price', None, type=int)
+    new_building = request.args.get('new_building')
+    page = request.args.get('page', 1, type=int)
+
+    if oblast_district:
+
+        ads = ads.filter(
+            Advertisement.oblast_district == oblast_district
+        )
+
+    if min_price:
+
+        ads = ads.filter(
+            Advertisement.price >= min_price
+        )
+
+    if max_price:
+
+        ads = ads.filter(
+            Advertisement.price <= max_price
+        )
+
+    if new_building:
+
+        ads = ads.filter(
+            or_(
+                Advertisement.under_construction == true(),
+                Advertisement.construction_year >= date.today().year - 2
+            )
+        )
 
     per_page = 5
-
-    ads = Advertisement.query.filter(Advertisement.active == True)
-
     ads = ads.paginate(page, per_page, False)
 
-    return render_template('ads_list.html', ads=ads)
+    return render_template(
+        'ads_list.html',
+        ads=ads,
+        min_price=min_price,
+        max_price=max_price,
+        new_building=new_building
+    )
 
 
 if __name__ == "__main__":
