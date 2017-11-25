fdfde8dbf512707d652e3d88ce253758b3df0055
sergei <sergei@Mac-mini-sergei.local>
2017-11-25 14:50:32 +0300

    add db indexes

--- models.py
+++ models.py
@@ -4,12 +4,19 @@ from app import db
 class Advertisement(db.Model):
 
     __tablename__ = 'advertisements'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=False,
+        index=True
+    )
+    
     settlement = db.Column(db.String(127))
     under_construction = db.Column(db.Boolean)
     description = db.Column(db.Text)
     price = db.Column(db.Integer)
-    oblast_district = db.Column(db.String(127))
+    oblast_district = db.Column(db.String(127), index=True)
     living_area = db.Column(db.Float)
     has_balcony = db.Column(db.Boolean)
     address = db.Column(db.String(255))
